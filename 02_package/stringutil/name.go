f948ea43883f03a1187445b210e97bd1de0f096d
scpeterson <tortilla.curtain@gmail.com>
2018-02-20 08:17:27 -0500

    Added package lesson

--- 02_package/stringutil/name.go
+++ 02_package/stringutil/name.go
@@ -1,1 +1,3 @@
 package stringutil
+
+var MyName = "Todd"
--- 02_package/stringutil/reverse.go
+++ 02_package/stringutil/reverse.go
@@ -1,1 +1,6 @@
+// Package stingutil contains utility functions for working with
 package stringutil
+// Reverse returns is argument string reversed rune-wise left to
+func Reverse(s string) string {
+	return reverseTwo(s)
+}
